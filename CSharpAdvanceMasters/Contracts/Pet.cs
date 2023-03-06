f4e46ca55da90c0973e986481b63bc38da67ad7d
chencerbo <124618132+chencerbo@users.noreply.github.com>
2023-03-07 01:39:49 +0800

    AssignmentNo2 (#2)
    * Assignment #2
    
    Signed-off-by: Cheneil Cerbo <cheneil.cerbo@softvision.com>
    
    * Removed assignment details
    
    Signed-off-by: Cheneil Cerbo <cheneil.cerbo@softvision.com>
    
    ---------
    
    Signed-off-by: Cheneil Cerbo <cheneil.cerbo@softvision.com>

---
+++ CSharpAdvanceMasters/Contracts/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public abstract class Pet: IPet
+    {
+        public abstract string Name { get; }
+        public abstract int Age { get; }
+
+        public abstract void Talk();
+        public void Walk() 
+        {
+            Console.WriteLine("Walking...");
+        }
+        public void Run() 
+        {
+            Console.WriteLine("Running...");
+        }
+    }
+}
---
+++ CSharpAdvanceMasters/Pets/Bird.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using System.ComponentModel;
+
+namespace Pets
+{
+    [DisplayName("Bird")]
+    public class Bird : Pet
+    {
+        public override string Name => "Tweety";
+
+        public override int Age => 4;
+
+        public override void Talk()
+        {
+            Console.WriteLine("Tweet...");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Flying...");
+        }
+    }
+}
