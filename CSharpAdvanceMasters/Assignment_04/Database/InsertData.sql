742eae541435c4efefc2503ee021947236404e14
Cheneil Cerbo <cheneil.cerbo@softvision.com>
2023-03-13 10:01:19 +0800

    Assignment #4 - LINQ
    Signed-off-by: Cheneil Cerbo <cheneil.cerbo@softvision.com>

---
+++ CSharpAdvanceMasters/Assignment_04/Database/InsertData.sql
@@ -0,0 +1,29 @@
+﻿INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('Captain America: The First Avenger', 2011, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('Captain Marvel', 2019, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('Iron Man', 2008, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('Iron Man 2', 2010, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('The Incredible Hulk', 2008, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted)  
+VALUES ('Thor', 2011, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('The Avengers', 2012, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted) 
+VALUES ('Iron Man 3', 2013, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted)
+VALUES ('Thor: The Dark World', 2011, 0, 0)
+
+INSERT INTO [Video] (Title, Year, IsRented, IsDeleted)
+VALUES ('Captain America: The Winter Soldier', 2014, 0, 0)
---
+++ CSharpAdvanceMasters/Assignment_04/Service/Interface/IVideoService.cs
@@ -0,0 +1,13 @@
+﻿using Assignment_04.Model;
+
+namespace Assignment_04.Service.Interface
+{
+    public interface IVideoService
+    {
+        public IEnumerable<Video> GetAllVideos();
+        public IEnumerable<Video> GetVideoByYear(int year);
+        public bool AddVideo(string title, int year);
+        public bool RentVideo(int? videoId, bool isRented);
+        public bool DeleteVideo(int? videoId, bool isDeleted);
+    }
+}
